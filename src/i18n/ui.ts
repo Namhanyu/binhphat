bdb2b373d0be89ad685559c93e1996c26ab49007
bar9ain <bar9ain@gmail.com>
2025-08-16 10:05:11 +0700

    section 3

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,11 +8,17 @@ export const defaultLang = "vi";
 export const ui = {
   vi: {
     "about.title": "Giới thiệu\nvề Bình Phát",
-    "about.content": "Công ty TNHH Xây dựng và Cơ điện Bình Phát xin gửi lời chúc sức khỏe và lời chào trân trọng nhất đến toàn thể Quý khách hàng!\n\nCông ty TNHH Xây dựng và Cơ điện Bình Phát được thành lập vào năm 2013, trên cơ sở các thành viên dày dặn kinh nghiệm và tâm huyết với nghề. Những năm gần đây, Bình Phát Construction không ngừng lớn mạnh, trưởng thành nhanh chóng và tự hào là đơn vị uy tín trong lĩnh vực xây dựng và cơ điện tại Việt Nam.",
+    "about.content":
+      "Công ty TNHH Xây dựng và Cơ điện Bình Phát xin gửi lời chúc sức khỏe và lời chào trân trọng nhất đến toàn thể Quý khách hàng!\n\nCông ty TNHH Xây dựng và Cơ điện Bình Phát được thành lập vào năm 2013, trên cơ sở các thành viên dày dặn kinh nghiệm và tâm huyết với nghề. Những năm gần đây, Bình Phát Construction không ngừng lớn mạnh, trưởng thành nhanh chóng và tự hào là đơn vị uy tín trong lĩnh vực xây dựng và cơ điện tại Việt Nam.",
     "about.button": "Về chúng tôi",
     "about.history": "Lịch sử\nhình thành",
     "about.profile": "Profile\nBình Phát",
+    "metrics.title": "Chất lượng xứng đáng với niềm tin",
+    "metrics.employees": "Nhân sự",
+    "metrics.years": "Năm hoạt động",
+    "metrics.projects": "Dự án",
+    "metrics.billion": "Tỷ đồng",
+    "metrics.note": "(Doanh thu 2024)",
   },
-  en: {
-  },
+  en: {},
 } as const;
