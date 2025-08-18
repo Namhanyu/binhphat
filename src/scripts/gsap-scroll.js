20acbec7d8296e05456993f7449e645f638a8b08
bar9ain <bar9ain@gmail.com>
2025-08-18 20:35:46 +0700

    operations - equipment

--- src/scripts/gsap-scroll.js
+++ src/scripts/gsap-scroll.js
@@ -281,6 +281,7 @@ export function initScrollAnimations() {
   // OPERATION FIELDS
   initSectionAnimations("#construction");
   initSectionAnimations("#electromechanical");
+  initSectionAnimations("#equipment");
 
   window.addEventListener("load", () => ScrollTrigger.refresh());
 }
